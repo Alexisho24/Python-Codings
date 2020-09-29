c06e7a7d4792ffdeba4fea2fb00de7a5e39e9bb3
Alexisho24 <69836525+Alexisho24@users.noreply.github.com>
2020-09-29 14:15:28 +0800

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,61 @@
+print("Title of program: Encouragement bot")
+print()
+while True:
+  description = input("Could you describe how you feel in a sentence?")
+
+  list_of_words = description.split()
+
+  feelings_list = []
+  encouragement_list = []
+  counter = 0
+  
+  for each_word in list_of_words:
+    
+    if each_word == "sad":
+      feelings_list.append("sad")
+      encouragement_list.append("that tomorrow will be a better day")
+      counter += 1
+    if each_word == "happy":
+      feelings_list.append("happy")
+      encouragement_list.append("to keep smiling")
+      counter += 1
+    if each_word == "tired":
+      feelings_list.append("tired")
+      encouragement_list.append("that you are stronger than you think")
+      counter += 1
+      
+    if each_word == "angry":
+      feelings_list.append("angry")
+      encouragement_list.append("that there are times when we get angry,everything will be alright")
+      counter += 1
+      
+    if each_word == "nervous":
+      feelings_list.append("nervous")
+      encouragement_list.append("that you have done your best,why not try to think about something happy")
+      counter += 1
+
+  if counter == 0:
+    
+      output = "Sorry I don't really understand. Please use different words?"
+
+  elif counter == 1:
+    
+      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember "+ encouragement_list[0] + "! Hope you feel better :)"  
+
+  else:
+
+    feelings = ""    
+    for i in range(len(feelings_list)-1):
+      feelings += feelings_list[i] + ", "
+    feelings += "and " + feelings_list[-1]
+    
+    encouragement = ""    
+    for j in range(len(encouragement_list)-1):
+      encouragement += encouragement_list[i] + ", "
+    encouragement += "and " + encouragement_list[-1]
+
+    output = "It seems that you are feeling quite " + feelings + ". Please always remember "+ encouragement + "! Hope you feel better :)"
+
+  print()
+  print(output)
+  print()
